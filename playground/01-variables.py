8bc4eb3d035ee2e9341d73d69288fd7230a449c1
siriyaparn <siriyasriboonma@gmail.com>
2024-06-25 00:07:20 +0700

    initial commit

---
+++ playground/01-variables.py
@@ -0,0 +1,17 @@
+# Learning Strings
+print("Working with strings")
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+print("\nPrint the length of the name")
+# long code and hard to read
+print(len(input("What is your name?")))
+
+
+# use variables to make code easier to read
+name = input("\nWhat is your name?")
+length = len(name)
+print(length)
