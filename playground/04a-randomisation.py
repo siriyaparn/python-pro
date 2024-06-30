37b83bcbf2d21c111efef75d0e57dba0633a60aa
siriyaparn <siriyasriboonma@gmail.com>
2024-06-30 23:05:19 +0700

    initial commit

---
+++ playground/04a-randomisation.py
@@ -0,0 +1,14 @@
+# Import module
+import random
+
+# Create random integer
+random_integer = random.randint(1, 10) 
+print(random_integer)
+
+# Create random float
+random_float = random.random()
+print(random_float)     # print random number between 0.000... to 0.999...
+
+# Create random between 0 and 5
+random_decimal = random.random() * 5
+print(random_decimal)
