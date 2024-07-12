d1bc943d7478be05a1ec6bdf9a03f473bcc28c96
siriyaparn <siriyasriboonma@gmail.com>
2024-07-12 23:08:09 +0700

    feat: create an empty list

--- playground/07a-hangman.py
+++ playground/07a-hangman.py
@@ -1,12 +1,20 @@
 # Import module
 import random
 
-# Step 1: Picking random words and checking answers
+# Picking random words and checking answers
 # Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-#print(chosen_word)
+print(chosen_word)
+
+# Create an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+for i in range(len(chosen_word)):
+    display.append("_")
+print(display)
 
 # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter (Please input only one letter): ").lower()
@@ -20,3 +28,4 @@ for letter in range(len(chosen_word)):
         print("Right")
     else:
         print("Wrong")
+        
