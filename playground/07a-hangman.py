ad855ff408e358f34cbbfc26fa13b1463f96b9fa
siriyaparn <siriyasriboonma@gmail.com>
2024-07-13 22:28:48 +0700

    feat: check if the player won

--- playground/07a-hangman.py
+++ playground/07a-hangman.py
@@ -1,38 +1,39 @@
 # Import module
 import random
 
-# Picking random words and checking answers
-# Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# Randomly choose a word from the word_list
 word_list = ["aardvark", "baboon", "camel"]
-
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
 # Testing code
 print(f"Pssst, the solution word is {chosen_word}")
 
-# Create an empty List called display.
-# For each letter in the chosen_word, add a "_" to 'display'.
-# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"].
+# Create an empty List
 display = []
-word_length = len(chosen_word)
-
 for _ in range(word_length):
     display.append("_")
 print(display)
 
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter (Please input only one letter): ").lower()
-while len(guess) > 1 :
+# Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in 
+# the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+end_of_game = False
+
+while not end_of_game:
+    # Input guess a letter
     guess = input("Guess a letter (Please input only one letter): ").lower()
-#print(guess)
+    while len(guess) > 1 :
+        guess = input("Guess a letter (Please input only one letter): ").lower()
+    #print(guess)
 
-# Check if the letter the user guessed (guess) is one of the letters in the chosen_word
-# Loop through each position in the chosen_word;
-# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter 
+    # Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter 
+    # Print display
+    print(display)   
 
-# Print display
-print(display)        
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
