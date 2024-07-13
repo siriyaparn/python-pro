2158a1c95c1e6f9875f5f8cf7a252852ca859680
siriyaparn <siriyasriboonma@gmail.com>
2024-07-13 22:03:10 +0700

    feat: replace blanks with guesses

--- playground/07a-hangman.py
+++ playground/07a-hangman.py
@@ -6,13 +6,16 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
+# Testing code
+print(f"Pssst, the solution word is {chosen_word}")
 
 # Create an empty List called display.
 # For each letter in the chosen_word, add a "_" to 'display'.
-# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"].
 display = []
-for i in range(len(chosen_word)):
+word_length = len(chosen_word)
+
+for _ in range(word_length):
     display.append("_")
 print(display)
 
@@ -23,9 +26,13 @@ while len(guess) > 1 :
 #print(guess)
 
 # Check if the letter the user guessed (guess) is one of the letters in the chosen_word
-for letter in range(len(chosen_word)):
-    if guess == chosen_word[letter]:
-        print("Right")
-    else:
-        print("Wrong")
-        
+# Loop through each position in the chosen_word;
+# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter 
+
+# Print display
+print(display)        
