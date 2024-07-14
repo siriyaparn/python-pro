160f99884e0d900b7e3125c928962e112c1abded
siriyaparn <siriyasriboonma@gmail.com>
2024-07-15 00:47:43 +0700

    feat: keep track of player's lives

--- playground/07a-hangman.py
+++ playground/07a-hangman.py
@@ -1,10 +1,70 @@
 # Import module
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 # Randomly choose a word from the word_list
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+
+# Create a variable called 'lives' to keep track of the number of lives left -> set 'lives' to equal 6.
+lives = 6
 
 # Testing code
 print(f"Pssst, the solution word is {chosen_word}")
@@ -13,7 +73,7 @@ print(f"Pssst, the solution word is {chosen_word}")
 display = []
 for _ in range(word_length):
     display.append("_")
-print(display)
+#print(display)
 
 # Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in 
 # the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
@@ -30,10 +90,23 @@ while not end_of_game:
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
-            display[position] = letter 
-    # Print display
-    print(display)   
+            display[position] = letter
+            #print(display)  
+
+    # If guess is not a letter in the chosen_word, then reduce 'lives' by 1. 
+    # If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
 
     if "_" not in display:
         end_of_game = True
         print("You win.")
+
+    # Print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
