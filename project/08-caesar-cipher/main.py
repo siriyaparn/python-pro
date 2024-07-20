dada999a4a581cda8af465227a7fc029382c84b7
siriyaparn <siriyasriboonma@gmail.com>
2024-07-20 23:21:34 +0700

    improve user experience

--- project/08-caesar-cipher/main.py
+++ project/08-caesar-cipher/main.py
@@ -1,3 +1,6 @@
+from art import logo
+
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -7,13 +10,28 @@ def caeser(start_text, shift_amount, cipher_direction):
     if cipher_direction == "decode":
         shift_amount *= -1
     for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += letter
     print(f"The {cipher_direction}d text is {end_text}")
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# Restart the cipher program?
+should_end = False
+while not should_end:
+    # User input
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26      # if the user enters a shift that is greater than the number of letters in the alphabet
 
-caeser(start_text = text, shift_amount = shift, cipher_direction = direction)
+    # Call ceasar function
+    caeser(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+    # Ask if user want to restart
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
