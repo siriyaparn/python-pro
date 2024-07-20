2cd2db2ad296c6f8c1d3594545aa742055841182
siriyaparn <siriyasriboonma@gmail.com>
2024-07-20 22:28:36 +0700

    rename a file

--- project/08-caesar-cipher/main.py
+++ project/08-caesar-cipher/main.py
@@ -1,10 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 #Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 def caeser(start_text, shift_amount, cipher_direction):
     end_text = ""
@@ -15,5 +11,9 @@ def caeser(start_text, shift_amount, cipher_direction):
         new_position = position + shift_amount
         end_text += alphabet[new_position]
     print(f"The {cipher_direction}d text is {end_text}")
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 caeser(start_text = text, shift_amount = shift, cipher_direction = direction)
