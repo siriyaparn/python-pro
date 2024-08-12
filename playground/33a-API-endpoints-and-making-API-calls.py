13cd139e220cf040fbfb3dba10d8e64143968ce3
siriyaparn <90504906+siriyaparn@users.noreply.github.com>
2024-08-12 22:34:22 +0700

    feat: http status

--- playground/33a-API-endpoints-and-making-API-calls.py
+++ playground/33a-API-endpoints-and-making-API-calls.py
@@ -1,6 +1,7 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
-# output: <Response [200]>
-
+if response.status_code == 404:
+  raise Exception("That resource does not exist.")
+elif response.status_code == 401:
+  raise Exception("You are not authorised to access this data.")
