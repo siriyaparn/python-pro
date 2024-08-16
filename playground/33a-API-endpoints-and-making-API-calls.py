deeef4d5147a433f6d77d9622db02f570c356ff8
siriyaparn <90504906+siriyaparn@users.noreply.github.com>
2024-08-16 23:56:51 +0700

    feat: raises an HTTPError exception

--- playground/33a-API-endpoints-and-making-API-calls.py
+++ playground/33a-API-endpoints-and-making-API-calls.py
@@ -1,7 +1,9 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-if response.status_code == 404:
-  raise Exception("That resource does not exist.")
-elif response.status_code == 401:
-  raise Exception("You are not authorised to access this data.")
+# Raises an HTTPError exception
+response.raise_for_status()
+
+data = response.json()
+print(data)
+# Output: {'timestamp': 1723827377, 'iss_position': {'latitude': '31.9354', 'longitude': '-53.7170'}, 'message': 'success'}
