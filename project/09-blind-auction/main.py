0370777ba1b433d10ddeb2be36862fc062a167c3
siriyaparn <siriyasriboonma@gmail.com>
2024-10-24 22:13:51 +0700

    feat: blind auction

--- project/09-blind-auction/main.py
+++ project/09-blind-auction/main.py
@@ -1,2 +1,16 @@
-name = input("What is your name?: ")
-bid = input("What is your bid?: ")
+from art import logo
+print(logo)
+
+bids = {}
+# Whether if new bids need to be added
+should_continue = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+
+continue_bidding = True
+while continue_bidding:
+    # input
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+
+    # Save data into dictionary {name:price}
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'. \n")
