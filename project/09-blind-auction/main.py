649b5df7c3a18cc03e08d53d4fce94ee1fefe360
siriyaparn <siriyasriboonma@gmail.com>
2024-10-29 23:08:41 +0700

    feat: compare bids in dictionary

--- project/09-blind-auction/main.py
+++ project/09-blind-auction/main.py
@@ -1,10 +1,18 @@
 from art import logo
 print(logo)
 
-bids = {}
-# Whether if new bids need to be added
-should_continue = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+# Compare bids in dictionary
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
 
+bids = {}
 continue_bidding = True
 while continue_bidding:
     # input
@@ -13,8 +21,12 @@ while continue_bidding:
 
     # Save data into dictionary {name:price}
     bids[name] = price
+
+    # Whether if new bids need to be added
     should_continue = input("Are there any other bidders? Type 'yes' or 'no'. \n")
-
     if should_continue == 'no':
-
-# Compare bids in dictionary
+        continue_bidding = False
+        find_highest_bidder(bids)
+        print(bids)
+    elif should_continue == "yes":
+        print("\n" * 30)
