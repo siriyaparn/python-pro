1e6de95f8237aea701fc341d91eaa07a56b96db5
siriyaparn <siriyasriboonma@gmail.com>
2024-10-30 22:41:08 +0700

    initial commit

---
+++ playground/10b-name-format.py
@@ -0,0 +1,7 @@
+def format_name(f_name, l_name):
+    f_name + l_name
+    return (f_name + " " + l_name)
+
+f_name = input("Please input your first name: ")
+l_name = input("Please input your last name: ")
+print(format_name())
