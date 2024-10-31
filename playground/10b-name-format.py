cc1b4f64723d4b6621dd7ec1bf28ecb093059b91
siriyaparn <siriyasriboonma@gmail.com>
2024-10-31 23:01:51 +0700

    feat: create name format

--- playground/10b-name-format.py
+++ playground/10b-name-format.py
@@ -1,7 +1,9 @@
 def format_name(f_name, l_name):
-    f_name + l_name
-    return (f_name + " " + l_name)
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return(f"{formated_f_name} {formated_l_name}")
 
-f_name = input("Please input your first name: ")
-l_name = input("Please input your last name: ")
-print(format_name())
+# Input 
+f_name = input("First name: ")
+l_name = input("Last name: ")
+format_name(f_name, l_name)
