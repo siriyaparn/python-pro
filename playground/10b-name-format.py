8b2b3ab61907c61fc4d8373424da2f5af8f2c0f6
siriyaparn <siriyasriboonma@gmail.com>
2024-11-03 23:08:37 +0700

    feat: multiple return value

--- playground/10b-name-format.py
+++ playground/10b-name-format.py
@@ -1,9 +1,12 @@
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You did not provide valid inputs"
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return(f"{formated_f_name} {formated_l_name}")
+    print("This will be not printed.") # This line will not be printed, return tell the computer that this is the end of function
 
 # Input 
-f_name = input("First name: ")
-l_name = input("Last name: ")
+f_name = input("What is your first name? ")
+l_name = input("What is your last name? ")
 format_name(f_name, l_name)
