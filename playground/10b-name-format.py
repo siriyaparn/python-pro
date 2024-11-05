0ae28fb67ff15c23d3601db6f69199ae1f5356f0
siriyaparn <siriyasriboonma@gmail.com>
2024-11-05 22:44:02 +0700

    add Docstrings

--- playground/10b-name-format.py
+++ playground/10b-name-format.py
@@ -1,4 +1,5 @@
 def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of the name"""
     if f_name == "" or l_name == "":
         return "You did not provide valid inputs"
     formated_f_name = f_name.title()
