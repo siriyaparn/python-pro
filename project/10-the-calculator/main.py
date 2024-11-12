c1987966f52c562307d61be46c08e83db74bafdc
siriyaparn <siriyasriboonma@gmail.com>
2024-11-12 23:07:00 +0700

    feat: create an operation dictionary and input

--- project/10-the-calculator/main.py
+++ project/10-the-calculator/main.py
@@ -9,3 +9,21 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
+
+# Add functions into a dictionary as the value
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+# Use a dictionary operations to perform the calculations
+first_number = float(input("What is the first number?: "))
+
+# Print the example of symbol
+for symbol in operations:
+    print(symbol)
+
+operation_synbol = input("Pick an operation: ")
+second_number = float(input("What is the next number?: "))
