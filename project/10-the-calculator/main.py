9995bd2ef04270e4d8fb3b763dcc8faa8809dcd8
siriyaparn <siriyasriboonma@gmail.com>
2024-11-17 22:50:40 +0700

    feat: create calculator function

--- project/10-the-calculator/main.py
+++ project/10-the-calculator/main.py
@@ -1,4 +1,7 @@
-# Create functions
+
+from art import logo
+
+# Create operation functions
 def add(n1, n2):
     return n1 + n2
 
@@ -19,16 +22,30 @@ operations = {
     "/": divide
 }
 
-# Use a dictionary operations to perform the calculations
-# Input
-first_number = float(input("What is the first number?: "))
-for symbol in operations:      # Print the example of symbol
-    print(symbol)
-operation_synbol = input("Pick an operation: ")
-second_number = float(input("What is the next number?: "))
+# Create calculator function
+def calculator():
+    print(logo)
+    should_calculate = True
+    first_number = float(input("What is the first number?: "))
 
-# Call function
-answer = operations[operation_synbol](first_number, second_number)
-print(f"{first_number} {operation_synbol} {second_number} = {answer}")
+    while should_calculate:
+        
+        for symbol in operations:      # Print the example of symbol
+            print(symbol)
+        operation_synbol = input("Pick an operation: ")
+        second_number = float(input("What is the next number?: "))
 
-choice = input(f"Type 'y' to continue calculating {answer}, or type 'n' to start a new calculation.")
+        # Call function
+        answer = operations[operation_synbol](first_number, second_number)
+        print(f"{first_number} {operation_synbol} {second_number} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating {answer}, or type 'n' to start a new calculation.")
+        if choice == 'y':
+            first_number = answer
+        else:
+            should_calculate = False
+            print("/n" * 20)
+            calculator()
+
+# Call function calculator
+calculator()
