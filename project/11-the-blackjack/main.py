043ed41c91197e931adc70f5844a4c775ce80a0c
siriyaparn <siriyasriboonma@gmail.com>
2024-11-22 22:23:41 +0700

    feat: return a random card

--- project/11-the-blackjack/main.py
+++ project/11-the-blackjack/main.py
@@ -1,6 +1,13 @@
+# Import module
+import random
 from art import logo
-print(logo)
 
 play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+if play == 'y':
+    print(logo)
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# Create function to return a random card
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]  # Ace = 11
+    card = random.choice(cards)
+    return card
