c53369b7a23c55da990d694a19f46d27f8bdae90
siriyaparn <siriyasriboonma@gmail.com>
2024-11-30 21:12:41 +0700

    feat: create condition if gane continue

--- project/11-the-blackjack/main.py
+++ project/11-the-blackjack/main.py
@@ -42,6 +42,12 @@ computer_score = calculate_score(computer_cards)
 print(f"Your cards : {user_cards} , current score: {user_score}")
 print(f"Computer's first card: {computer_cards[0]}")
 
-# End game condition
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
+# Game condition
+if user_score == 0 or computer_score == 0 or user_score > 21: 
+    is_game_over = True   # End game
+else:
+    user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+    if user_should_deal == 'y':
+        user_cards.append(deal_card)
+    else:
+        is_game_over = True
