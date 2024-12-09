40753d11ed6c0594a640f973f4c9729a7b75e610
siriyaparn <siriyasriboonma@gmail.com>
2024-12-09 22:08:46 +0700

    feat: create compare function

--- project/11-the-blackjack/main.py
+++ project/11-the-blackjack/main.py
@@ -26,11 +26,21 @@ def calculate_score(cards):
     return sum(cards)
 
 # Create compare function
-def compare(user_score, computer_score):
-    if user_score == computer_score:
+def compare(u_score, c_score):
+    if u_score == c_score:
         return "Draw"
-    elif computer_score == 0:
+    elif c_score == 0:
         return "Lose, opponent has Blackjack"
+    elif u_score == 0:
+        return "Win with a Blackjack"
+    elif u_score > 21:
+        return "You went over. You lose"
+    elif c_score > 21:
+        return "Opponent went over. You win"
+    elif u_score > c_score:
+        return "You win"
+    else:
+        return "You lose"
 
 # Declear parameters
 user_cards = []
