8cd1a5f4a4010fc90aaf9349d573e0a011152d90
siriyaparn <siriyasriboonma@gmail.com>
2024-12-30 22:33:26 +0700

    initial commit

---
+++ playground/12e-global-const.py
@@ -0,0 +1,9 @@
+# Global constants
+## You can define global constants in your code flie for easy access.
+## Their job is meant to be "set and forget" so you can use their values but never need ti mofy them.
+
+PI = 3.14159
+GOOGLE_URL = "https://www.google.com"
+
+def my_func():
+    GOOGLE_URL
