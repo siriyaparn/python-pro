6f2874b2f14c7ca380d38930133a0b975415b28c
siriyaparn <siriyasriboonma@gmail.com>
2025-01-01 22:37:32 +0700

    initial commit

--- project/12-the-number-guessing-game/main.py
+++ project/12-the-number-guessing-game/main.py
@@ -1,3 +1,4 @@
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
-print("Pssst, the correct answer is 100.")
+
+mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
