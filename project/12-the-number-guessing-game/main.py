35897bbf4f392bff5649627d88831d093c4974ea
siriyaparn <siriyasriboonma@gmail.com>
2025-01-02 22:42:26 +0700

    create random number

--- project/12-the-number-guessing-game/main.py
+++ project/12-the-number-guessing-game/main.py
@@ -1,4 +1,9 @@
+# Import module
+import random
+from art import logo
+
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
 mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
+random_number = random.randint(1,100)
