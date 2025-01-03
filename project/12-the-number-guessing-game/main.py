b2e6ceb45332d92a81e14345c556a545b3934020
siriyaparn <siriyasriboonma@gmail.com>
2025-01-03 12:46:27 +0700

    add attempt

--- project/12-the-number-guessing-game/main.py
+++ project/12-the-number-guessing-game/main.py
@@ -7,3 +7,8 @@ print("I'm thinking of a number between 1 and 100.")
 
 mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
 random_number = random.randint(1,100)
+
+if mode == "easy":
+    attempt = 10
+else:
+    attempt = 5
