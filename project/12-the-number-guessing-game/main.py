248fac098b313f28b84864152d709f2de91f9808
siriyaparn <siriyasriboonma@gmail.com>
2025-01-07 23:11:41 +0700

    add logo

--- project/12-the-number-guessing-game/main.py
+++ project/12-the-number-guessing-game/main.py
@@ -2,13 +2,18 @@
 import random
 from art import logo
 
+print("logo")
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 
 mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
 random_number = random.randint(1,100)
 
-if mode == "easy":
+def easy():
     attempt = 10
+
+
+if mode == "easy":
+    easy
 else:
     attempt = 5
