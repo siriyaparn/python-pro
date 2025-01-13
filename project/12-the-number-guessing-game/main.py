737eff1c894184bfc3f39651f39fb62b2538ff56
siriyaparn <siriyasriboonma@gmail.com>
2025-01-13 22:16:42 +0700

    feat: random number

--- project/12-the-number-guessing-game/main.py
+++ project/12-the-number-guessing-game/main.py
@@ -3,11 +3,15 @@ import random
 from art import logo
 
 print(logo)
+
+# Choosing random number between 1 and 100
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
+random_number = random.randint(1,100)
+
 
 mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
-random_number = random.randint(1,100)
+
 
 def easy():
     attempt = 10
