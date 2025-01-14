cf1deb0b7bac19d48c1d71fc9f4a65ed9429fe90
siriyaparn <siriyasriboonma@gmail.com>
2025-01-14 21:36:56 +0700

    feat: set difficulty

--- project/12-the-number-guessing-game/main.py
+++ project/12-the-number-guessing-game/main.py
@@ -4,6 +4,9 @@ from art import logo
 
 print(logo)
 
+EASY_MODE_TURNS = 10
+HARD_MODE_TURNS = 5
+
 # Function to check answer
 def check_answer(user_guess, actual_answer):
     if user_guess > actual_answer:
@@ -14,6 +17,14 @@ def check_answer(user_guess, actual_answer):
         attempt -= 1
     else:
         print(f"You got it! The answer was {actual_answer}")
+
+# Function to set difficulty
+def set_difficulty():
+    mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if mode == 'easy':
+        turns = EASY_MODE_TURNS
+    else:
+        turns = HARD_MODE_TURNS
 
 # Choosing random number between 1 and 100
 print("Welcome to the Number Guessing Game!")
