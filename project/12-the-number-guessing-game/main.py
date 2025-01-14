c09a30991c1a54c45f98198ca509ad9dce286da7
siriyaparn <siriyasriboonma@gmail.com>
2025-01-14 21:06:23 +0700

    feat: check answer

--- project/12-the-number-guessing-game/main.py
+++ project/12-the-number-guessing-game/main.py
@@ -4,36 +4,18 @@ from art import logo
 
 print(logo)
 
+# Function to check answer
+def check_answer(user_guess, actual_answer):
+    if user_guess > actual_answer:
+        print("Too high.")
+        attempt -= 1
+    elif user_guess < actual_answer:
+        print("Too low.")
+        attempt -= 1
+    else:
+        print(f"You got it! The answer was {actual_answer}")
+
 # Choosing random number between 1 and 100
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 random_number = random.randint(1,100)
-
-
-mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
-
-
-def easy():
-    attempt = 10
-    while attempt <= 10:
-        print(f"You have {attempt} attempts remaining to guess the number.")
-        guess = int(input("Make a guess: "))
-        if guess > random_number:
-            print("Too high.")
-            attempt -= 1
-        elif guess < random_number:
-            print("Too low.")
-            attempt -= 1
-        elif guess == random_number:
-            print("You win!")
-            break
-    if attempt == 0:
-        print("You lose.")
-
-def hard():
-    attempt = 5
-
-if mode == "easy":
-    easy()
-else:
-    hard()
