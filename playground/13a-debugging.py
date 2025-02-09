c855288409b1313e679215f72a0cd718a3d2027f
siriyaparn <siriyasriboonma@gmail.com>
2025-02-09 23:07:41 +0700

    explain code

--- playground/13a-debugging.py
+++ playground/13a-debugging.py
@@ -4,3 +4,6 @@ def my_function():
             print("You got it")
 
 my_function()
+
+# What is the for loop doing?
+## Ans: loops through all the numbers between 1 and 20, and then once that number reaches 20, it's supposed to print "You got it"
