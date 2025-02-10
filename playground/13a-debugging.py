04dac0d926f5bbce3bc73ba52962e5af60ebc999
siriyaparn <siriyasriboonma@gmail.com>
2025-02-10 22:36:48 +0700

    describe the problem

--- playground/13a-debugging.py
+++ playground/13a-debugging.py
@@ -5,5 +5,12 @@ def my_function():
 
 my_function()
 
+# Describe the problem
 # What is the for loop doing?
-## Ans: loops through all the numbers between 1 and 20, and then once that number reaches 20, it's supposed to print "You got it"
+## Ans: Loop through all the numbers between 1 and 20, and then once that number reaches 20, it's supposed to print "You got it"
+
+# When is the function meant to print "You got it"?
+## Ans: When it reaches 20
+
+# What are your assumptions about the vaule of it?
+## Ans: When we write 1, 20, it actually goes from 1 all the way up to 19, but not including 20. The problem here is that i actually never reaches 20, so this assumption is completely false.
