2ca6848be326431739fe3e6b0da70b29851a8137
siriyaparn <siriyasriboonma@gmail.com>
2025-02-17 23:53:43 +0700

    fix code

--- playground/13c-play-computer.py
+++ playground/13c-play-computer.py
@@ -3,4 +3,4 @@ year = int(input("What is your year of birth? "))
 if year > 1980 and year < 1994:
         print("You are a millennal.")
 elif year > 1994:
-        print("You are a Gen Z")
+        print("You are a Gen ")
