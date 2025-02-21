ae15b2bb1d1871beb71cf5bb56d1e120d6649636
siriyaparn <siriyasriboonma@gmail.com>
2025-02-21 23:46:12 +0700

    feat: create try except

--- playground/13d-fixing-error.py
+++ playground/13d-fixing-error.py
@@ -1,4 +1,7 @@
-age = int(input("How old are you? :"))
+try:
+    age = int(input("How old are you? :"))
+except ValueError:
+    print("You are typed in a an invalid number. Please try again with a numerical response such as 15.")
 
 if age > 18:
     print(f"You can drive at age {age}")
