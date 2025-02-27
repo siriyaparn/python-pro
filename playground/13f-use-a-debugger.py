cee99c6c1ada64989eee627616442b9310aa2f0a
siriyaparn <siriyasriboonma@gmail.com>
2025-02-27 22:54:17 +0700

    debug and fix code

--- playground/13f-use-a-debugger.py
+++ playground/13f-use-a-debugger.py
@@ -1,14 +1,14 @@
 import random
 import maths
 
-def mutate(a_list):
-    b_list = []
-    new_item = 0
+def mutate(a_list):     # a_list: [1, 2, 3, 5, 8, 13]
+    b_list = []         # b_list: [] 
+    new_item = 0        # new_item: 0 then 1 and so on.. 
     for item in a_list:
         new_item = item * 2
         new_item += random.randint(a:1, b:3)
         new_item = maths.add(new_item, item)
-    b_list.append(new_item)
+        b_list.append(new_item)
     print(b_list)
 
 mutate([1, 2, 3, 5, 8, 13])
