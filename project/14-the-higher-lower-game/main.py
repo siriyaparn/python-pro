136410169568d8f6478e303eae09048189a357a1
siriyaparn <siriyasriboonma@gmail.com>
2025-03-13 22:03:27 +0700

    feat: format the account data

--- project/14-the-higher-lower-game/main.py
+++ project/14-the-higher-lower-game/main.py
@@ -1,17 +1,24 @@
 # Import module
 import random
-import game_data
 from art import logo
+from game_data import data
 
 # Display art logo
 print(logo)
 
+def format_data(account):
+    """Format the account data into printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    print(f"{account_name}, a {account_descr}, form {account_country}")
+
 # Generate a random account from the game data
-account_a = random.choice(game_data.data)
-account_b = random.choice(game_data.data)
+account_a = random.choice(data)
+account_b = random.choice(data)
 if account_a == account_b:
-    ccount_b = random.choice(game_data.data)
-    
+    ccount_b = random.choice(data)
+
 print(f"Compare A: {account_a['name']}, a {account_a['description']}, from {account_a['country']}.")
 print("vs")
 print(f"Compare B: {account_b['name']}, a {account_b['description']}, from {account_b['country']}.")
