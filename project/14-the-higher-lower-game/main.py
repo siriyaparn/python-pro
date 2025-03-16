013f7c53024ee015bc993f6a98dd5891445d77f3
siriyaparn <siriyasriboonma@gmail.com>
2025-03-16 23:32:37 +0700

    feat: check if user is correct

--- project/14-the-higher-lower-game/main.py
+++ project/14-the-higher-lower-game/main.py
@@ -7,11 +7,19 @@ from game_data import data
 print(logo)
 
 def format_data(account):
-    """Format the account data into printable format."""
+    """Takes the account data and returns the printable format."""
     account_name = account["name"]
     account_descr = account["description"]
     account_country = account["country"]
     return(f"{account_name}, a {account_descr}, form {account_country}")
+
+def check_answer(user_guess, a_followers, b_followers):
+    """Takes a user's guess and the follower counts and returns if they got it right."""
+    if a_followers > b_followers: 
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
+
 
 # Generate a random account from the game data
 account_a = random.choice(data)
@@ -32,6 +40,6 @@ a_follow_account = account_a["follower_count"]
 b_follow_account = account_b["follower_count"]
 
 ## Use if statement to check if user is correct
-
+is_correct = check_answer(guess, a_follow_account, b_follow_account)
 
 # Give user feedback on their guess
