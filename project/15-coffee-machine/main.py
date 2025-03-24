66ced5850749763e541aa668a1897a628ce944b2
siriyaparn <siriyasriboonma@gmail.com>
2025-03-24 21:43:10 +0700

    add menu

--- project/15-coffee-machine/main.py
+++ project/15-coffee-machine/main.py
@@ -1,1 +1,28 @@
+# Menu
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "co,ffee": 18
+        },
+        "cost": 1.5
+    },
+    "latte": {
+        "ingredients":{
+            "water": 200,
+            "milk": 150,
+            "coffee": 24
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients":{
+            "water": 250,
+            "milk": 100,
+            "coffee": 24
+        },
+        "cost": 3.0,
+    },
+}
+
 # Create report
