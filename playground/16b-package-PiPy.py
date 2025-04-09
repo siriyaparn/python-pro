5905cb4e345f3fcb6f6330a1598ae1a3839bf0a7
siriyaparn <siriyasriboonma@gmail.com>
2025-04-10 00:12:02 +0700

    add rows/coulumns

--- playground/16b-package-PiPy.py
+++ playground/16b-package-PiPy.py
@@ -2,4 +2,21 @@ from prettytable import PrettyTable
 
 # Create object
 table = PrettyTable()
+
+# Set field names
+table.field_names = ["Pokemon Name", "Type"]
+
+# Add rows
+table.add_rows(
+    [
+        ["Pikachu", "Electric"],
+        ["Squirtle", "Water"],
+        ["Squirtle", "Fire"],
+    ]
+)
+
+# Add columns
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Squirtle"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
 print(table)
