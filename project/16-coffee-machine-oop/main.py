d4713df6812fe17cf22c149d7e3a0933fdb9c3a1
siriyaparn <siriyasriboonma@gmail.com>
2025-04-20 21:37:20 +0700

    feat: print report

--- project/16-coffee-machine-oop/main.py
+++ project/16-coffee-machine-oop/main.py
@@ -3,3 +3,8 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 # print report
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+coffee_maker.report()
+money_machine.report()
