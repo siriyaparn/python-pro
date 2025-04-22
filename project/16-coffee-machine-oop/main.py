c7e0dcb58628fed6a34f0d01cf9ae00a426a7fba
siriyaparn <siriyasriboonma@gmail.com>
2025-04-22 23:30:12 +0700

    create menu object

--- project/16-coffee-machine-oop/main.py
+++ project/16-coffee-machine-oop/main.py
@@ -2,11 +2,16 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# print report
+# Create objects
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
 
+# Print report
 coffee_maker.report()
 money_machine.report()
 
 # Check resources sufficients
+while is_on:
+    options = menu.get_items()
