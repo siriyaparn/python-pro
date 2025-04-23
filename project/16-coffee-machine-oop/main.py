9bbad74ced9e345756b01fa1d5f69afaa68b692f
siriyaparn <siriyasriboonma@gmail.com>
2025-04-23 23:21:24 +0700

    feat: print report

--- project/16-coffee-machine-oop/main.py
+++ project/16-coffee-machine-oop/main.py
@@ -9,9 +9,13 @@ menu = Menu()
 is_on = True
 
 # Print report
-coffee_maker.report()
-money_machine.report()
-
-# Check resources sufficients
 while is_on:
     options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
