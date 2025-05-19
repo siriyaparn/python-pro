57a6bce4856e76804c7f4d3a0e472ef710c0b183
siriyaparn <siriyasriboonma@gmail.com>
2025-05-19 08:48:47 +0700

    create Question class

--- project/17-quiz-game-oop/question_model.py
+++ project/17-quiz-game-oop/question_model.py
@@ -1,2 +1,7 @@
 class Question:
-    pass
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+new_q = Question("asdfg", "False")
+print(new_q.answer)
