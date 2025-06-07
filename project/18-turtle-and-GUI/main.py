ae310f3e98443313098a8f8e7d4b72688f8c63be
siriyaparn <siriyasriboonma@gmail.com>
2025-06-07 23:24:20 +0700

    turtle graphic

--- project/18-turtle-and-GUI/main.py
+++ project/18-turtle-and-GUI/main.py
@@ -2,7 +2,7 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("blue")
+timmy_the_turtle.color("red")
 
 screen = Screen()
 screen.exitonclick()
