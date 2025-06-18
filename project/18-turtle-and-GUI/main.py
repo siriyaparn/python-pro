6818bf0cac24271622f3ce62b02441f9da662b67
siriyaparn <siriyasriboonma@gmail.com>
2025-06-18 23:49:52 +0700

    challenge 3: drawing different shape

--- project/18-turtle-and-GUI/main.py
+++ project/18-turtle-and-GUI/main.py
@@ -21,7 +21,23 @@ for _ in range(10):
     tim.forward(10)
     tim.pendown
 
-# Challenge 3
+# Challenge 3: Drawing different shape
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchild", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+def draw_shape(nem_sides):
+    angle = 360/num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
 
 
 screen = Screen()
