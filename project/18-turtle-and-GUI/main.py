3ec18b13a4f09ef9f282efc88fdaa66f9f9ed658
siriyaparn <siriyasriboonma@gmail.com>
2025-06-24 23:49:26 +0700

    challenge 5: draw a spiograph

--- project/18-turtle-and-GUI/main.py
+++ project/18-turtle-and-GUI/main.py
@@ -63,6 +63,29 @@ for _ in range(200):
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
+# Challenge 5: Draw a spiograph
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    random_color = (r, g, b)
+    return random_color
+
+tim.speed("fastest")
+
+def draw_spiograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spiograph(5)
 
 screen = Screen() 
 screen.exitonclick()
