3f0df46250537ee808da1af2166bee5216188c3b
siriyaparn <siriyasriboonma@gmail.com>
2025-07-15 23:42:42 +0700

    make an etch-a-sketch app

--- project/19-order-function-event-listening/main.py
+++ project/19-order-function-event-listening/main.py
@@ -3,3 +3,9 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
+def move_forwards():
+    tim.forward(10)
+
+screen.listen()
+screen.onkey(move_forwards, "space")
+screen.exitonclick()
