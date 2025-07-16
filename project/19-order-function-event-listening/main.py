6636cd27ed78d16631f5701957be24ff9e41fac5
siriyaparn <siriyasriboonma@gmail.com>
2025-07-16 23:37:00 +0700

    make an etch-a-sketch app

--- project/19-order-function-event-listening/main.py
+++ project/19-order-function-event-listening/main.py
@@ -6,6 +6,21 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
 screen.listen()
-screen.onkey(move_forwards, "space")
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
 screen.exitonclick()
