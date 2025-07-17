99905b8743ee91703089646c0c1542e390a38849
siriyaparn <siriyasriboonma@gmail.com>
2025-07-17 23:28:15 +0700

    make an etch-a-sketch app

--- project/19-order-function-event-listening/main.py
+++ project/19-order-function-event-listening/main.py
@@ -17,10 +17,16 @@ def turn_right():
     new_heading = tim.heading() - 10
     tim.setheading(new_heading)
 
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 screen.listen()
 screen.onkey(move_forwards, "w")
 screen.onkey(move_backwards, "s")
 screen.onkey(turn_left, "a")
 screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
 screen.exitonclick()
