f9bbf65d37d4419620581e62c9b16e5875ee0063
siriyaparn <siriyasriboonma@gmail.com>
2025-07-23 23:37:17 +0700

    turtle racing

--- project/19-turtle-racing/main.py
+++ project/19-turtle-racing/main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
-screen.setup(500, 400)
+screen.setup(width=500, height=400)
+screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 
 screen.exitonclick()
